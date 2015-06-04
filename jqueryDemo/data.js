bc7032b2907c233e2d6672f52cce7433e3257823
Calvin Webster <calweb@gmail.com>
2015-06-04 13:21:48 -0400

    moar jquery

--- jqueryDemo/data.js
+++ jqueryDemo/data.js
@@ -1,3 +1,4 @@
+// our default post data
 var posts = [
   {
     title: "Lucius Title",
@@ -10,3 +11,27 @@ var posts = [
     content: "This is Kelsey's content.....lotsa text here."
   }
 ];
+
+var templates = {};
+
+// single post template
+templates.post = [
+  "<article class='post'>",
+  "<h3><%= title %></h3>",
+  "<p><%= content %></p>",
+  "<blockquote cite='http://calvin.io'>",
+  "<%= author %></blockquote>",
+  "<a href='' class='delete'>delete</a>",
+  "</article>"
+].join("");
+templates.boo = [
+  "<ul>",
+    "<li><%= name %></li>",
+  "</ul>"
+].join("");
+
+// about me page template
+templates.about = [
+  "<h1>More About Me</h1>",
+  "<p>ThiCompiles JavaScript templates into functions that can be evaluated for rendering. Useful for rendering complicated bits of HTML from JSON data sources. Template functions can both interpolate values, using something, as well as execute arbitrary JavaScript code, with something. If you wish to interpolate a value, and have it be HTML-escaped, use something. When you evaluate a template function, pass in a data object that has properties corresponding to the template's free variables. The settings argument should be a hash containing any _.templateSettings that should be overridden.</p>"
+].join("");
--- jqueryDemo/main.js
+++ jqueryDemo/main.js
@@ -1,64 +1,83 @@
-$(document).ready(function() {
-  // anything i do in here is safe to interact with the document
-  // when i say 'safe', i mean the dom exists at this point
-  //
-function addFour(number) {
-  console.log(typeof number);
-  return Number(number) + 4;
-}
+$(document).ready(function() { // The DOM is now ready to interact with.
 
-$('.addFour').click(function(event) {
-  event.preventDefault();
-  var amount = $('input[name="addfour"]').val();
-  var finalCalc = addFour(amount);
-  $('.total').text(finalCalc);
-});
+// 1.load the _.template with the template string
+var compiledTmpl = _.template($('#postTmpl').html());
+var tmplString = "";
 
-
-
-var template = "";
 posts.forEach(function(el) {
-  template += "<article class='post'><h3>"
-  + el.title
-  +"</h3><p>"
-  + el.content
-  + "</p>"
-  + "<blockquote cite='http://calvin.io'>"
-  + el.author
-  + "</blockquote><a href='' class='delete'>delete</a>"
-  + "</article>";
-
+  // console.log(compiledTmpl(el));
+  // 2. add the data to the compiled template
+  tmplString += compiledTmpl(el);
 
 });
 
-$('body').on('click', '.delete', function(e) {
+$('.content').append(tmplString);
+loadTemplate("about", {}, $('.about'));
+
+$('.content').on('click', '.delete', function(e) {
   e.preventDefault();
   $(this).closest("article").remove();
 });
 
+$('.content').on('click', 'button', function(evt) {
+  evt.preventDefault();
+  var $this = $(this);
+  console.log("this: ",this);
+  console.log("name", $(this).attr('name'));
+});
 
 
-$('.content').append(template);
+// SHORTHIAND EVENT for binding an event to an element
+// the callbackFn is a function that gets executed when the click happens on someElement
+// $('someElement').click(callbackFn);
 
-$('.newBlogSubmit').click(function(event) {
+// NORMAL EVENT using the .on() syntax - this is the preferrable way
+// @param {string} eventType The type of event (eg. click, submit, mouseover)
+// @param {function} callbackFn The callback function that executes when the event fires
+// $('someElement').on(eventType, callbackFn);
+
+/* DELEGATED EVENT
+** @param {string} eventType The type of event (eg. click, submit, mouseover)
+** @param {string} targetElement The target element that will trigger the event
+** @param {function} callbackFn The callback function that executes when the event fires
+** $('someElement').on(eventType, targetElement, callbackFn);
+*/
+
+
+// adds new post
+$('.newBlogSubmit').on('click', function (event) {
   event.preventDefault();
+  // build an object that looks like our original data
+  var newPost = {
+    title: $('input[name="title"]').val(),
+    content: $('textarea').val(),
+    author: $('input[name="author"]').val()
+  };
 
-  var addPostTmpl = "<article class='post'><h3>"
-  + $('input[name="title"]').val()
-  +"</h3><p>"
-  + $('textarea').val()
-  + "</p>"
-  + "<blockquote cite='http://calvin.io'>"
-  + $('input[name="author"]').val()
-  + "</blockquote><a href='' class='delete'>delete</a>"
-  + "</article>";
+   loadTemplate("post", newPost, $('.content'));
 
-  $('.content').prepend(addPostTmpl);
+  // clear form
   $('input, textarea').val("");
+});
+
+// displaying and hiding different pages by clicking the navigation
+$('.nav').on('click', 'a', function (event) {
+  event.preventDefault();
+  console.log("this element ",$(this).attr('rel'));
+  var clickedPage = $(this).attr('rel');
+  $(clickedPage).siblings().removeClass('active');
+  $(clickedPage).addClass('active');
+});
 
 });
 
+// small function to handle using the _.template function and putting it on the page
+// @param {string} name Name of the template from the templates object
+// @param {object} data The data you want to merge with your string template
+// @param {object} $target jQuery object used to append the compiled template with data to the page.
+function loadTemplate(name, data, $target) {
+  var tmpl = _.template(templates[name]);
+  $target.append(tmpl(data));
 
 
-
-});
+}
