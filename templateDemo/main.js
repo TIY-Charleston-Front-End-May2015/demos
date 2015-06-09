c7b8c690b044cd113a4a044cfc82df9a78ce8166
Calvin Webster <calweb@gmail.com>
2015-06-09 13:22:50 -0400

    some minor edits

--- templateDemo/main.js
+++ templateDemo/main.js
@@ -1,17 +1,3 @@
-
-
-var refinedReddit = _.map(redditData.data.children, function(el) {
-  return { title: el.data.title, author: el.data.author, score: el.data.score, url: el.data.url};
-});
-var redditTmpl = _.template($('#redditTmpl').html());
-
-_.each(refinedReddit, function (el) {
-  console.log(redditTmpl(el));
-  $('.reddits').append(redditTmpl(el));
-});
-
-// console.log("refined reddit", refinedReddit);
-
 var sampleReddit = {
   title: "This is my first reddit, gimme some gold!",
   author: "Calvin WEbster",
@@ -20,6 +6,24 @@ var sampleReddit = {
   isCool: true
 };
 
+// lets take the actual data from reddit and pare it down to be the same shape as the sampleReddit object.
+var refinedReddit = _.map(redditData.data.children, function(el) {
+  return {
+    title: el.data.title,
+    author: el.data.author,
+    score: el.data.score,
+    url: el.data.url
+    };
+});
+
+var redditTmpl = _.template($('#redditTmpl').html());
+
+_.each(refinedReddit, function (el) {
+  // console.log(redditTmpl(el));
+  $('.reddits').append(redditTmpl(el));
+});
+
+// console.log("refined reddit", refinedReddit);
 
 // _.times(100, function () {
 //   $('.reddits').append(redditTmpl(sampleReddit));
