68ff008ad1ae9a5cfad58040ec6f49db5d4e9c26
Calvin Webster <calweb@gmail.com>
2015-06-10 11:34:56 -0400

    added ajax intro

--- jqueryDemo/refactored.js
+++ jqueryDemo/refactored.js
@@ -5,33 +5,113 @@ $(document).ready(function () {
 
 
 var page = {
-  getTemplate: function (name) {
-    return templates[name];
-  },
-  posts: posts,
-  init: function (arguments) {
+
+  url: "http://tiy-fee-rest.herokuapp.com/collections/tacobell",
+  init: function () {
     page.initStyling();
     page.initEvents();
   },
-  initStyling: function (arguments) {
-    var compiled = _.template(page.getTemplate("boo"));
-    console.log(compiled({name: "calvin"}));
-    page.posts.forEach(function (el) {
-      page.loadTemplate("post", el, $('.content'));
-    });
+  initStyling: function () {
 
+    page.loadPosts();
   },
-  initEvents: function (arguments) {
+  initEvents: function () {
 
     $('.content').on('click', '.delete', page.deletePost);
     $('.newBlogSubmit').on('click', page.addPost);
     $('.nav').on('click', 'a', page.navPages);
     $('.aboutBlogPost').on('click', 'a', page.addPost);
 
+    $('.content').on('click', '.editPost', function (e) {
+      e.preventDefault();
+      $(this).next().toggleClass('active');
+    });
+
+    $('.content').on('click', '.submitEdit', function (e) {
+      e.preventDefault();
+      var $thisEditing = $(this).closest('.editing');
+      var postId = $(this).closest('article').data('id');
+      var updatedPost = {
+        title: $thisEditing.find('.editTitle').val(),
+        content: $thisEditing.find('.editContent').val(),
+        author: $thisEditing.find('.editAuthor').val()
+      };
+
+      page.updatePost(updatedPost, postId);
+
+
+    });
+
+
+  },
+
+  addOnePostToDOM: function (post) {
+    page.loadTemplate("post", post, $('.blog > .content'));
+  },
+  addAllPostsToDOM: function (postCollection) {
+    _.each(postCollection, page.addOnePostToDOM);
+  },
+  loadPosts: function () {
+
+    $.ajax({
+      url: page.url,
+      method: 'GET',
+      success: function (data) {
+        console.log(data);
+        page.addAllPostsToDOM(data);
+      },
+      error: function (err) {
+
+      }
+    });
+
+
+  },
+  createPost: function (newPost) {
+
+    $.ajax({
+      url: page.url,
+      method: 'POST',
+      data: newPost,
+      success: function (data) {
+
+        page.addOnePostToDOM(data);
+        console.log("success!!: ", data);
+      },
+      error: function (err) {
+        console.log("error ", err);
+      }
+    });
+
+  },
+  updatePost: function (editedPost, postId) {
+
+    $.ajax({
+      url: page.url + '/' + postId,
+      method: 'PUT',
+      data: editedPost,
+      success: function (data) {
+        $('.content').html('');
+        page.loadPosts();
+        
+      },
+      error: function (err) {}
+    });
+
+
   },
   deletePost: function(e) {
     e.preventDefault();
-    $(this).closest("article").remove();
+
+    $.ajax({
+      url: page.url + "/" + $(this).closest('article').data('id'),
+      method: 'DELETE',
+      success: function (data) {
+        console.log(this);
+        $('.content').html('');
+        page.loadPosts();
+      }
+    });
   },
   navPages: function (event) {
     event.preventDefault();
@@ -40,6 +120,7 @@ var page = {
     $(clickedPage).siblings().removeClass('active');
     $(clickedPage).addClass('active');
   },
+
   addPost: function (event) {
     event.preventDefault();
 
@@ -49,8 +130,7 @@ var page = {
       content: $('textarea').val(),
       author: $('input[name="author"]').val()
     };
-    console.log(newPost);
-     page.loadTemplate("post", newPost, $('.blog > .content'));
+    page.createPost(newPost);
 
     // clear form
     $('input, textarea').val("");
@@ -59,5 +139,8 @@ var page = {
     var compiledTmpl = _.template(page.getTemplate(tmplName));
 
     $target.append(compiledTmpl(data));
+  },
+  getTemplate: function (name) {
+    return templates[name];
   }
 };
--- jqueryDemo/templates.js
+++ jqueryDemo/templates.js
@@ -1,32 +1,22 @@
-// our default post data
-var posts = [
-  {
-    title: "Lucius Title",
-    author: "Calvin",
-    content: "This is calvins content.....lotsa text here."
-  },
-  {
-    title: "Kelsey Title",
-    author: "Kelsey",
-    content: "This is Kelsey's content.....lotsa text here."
-  },
-  {
-    title: "Nathan Title",
-    author: "Nathan",
-    content: "This is Kelsey's content.....lotsa text here."
-  }
-];
 
 var templates = {};
 
 // single post template
 templates.post = [
-  "<article class='post'>",
+  "<article class='post' data-id='<%= _id %>'>",
   "<h3><%= title %></h3>",
   "<p><%= content %></p>",
   "<blockquote cite='http://calvin.io'>",
   "<%= author %></blockquote>",
-  "<a href='' class='delete'>delete</a>",
+  "<a href='' class='delete btn btn-danger'>delete</a> | <a href='' class='editPost'>edit</a>",
+  "<div class='editing'>",
+  "<input type='text' class='editTitle' value='<%= title %>'>",
+  "<input type='text' class='editAuthor' value='<%= author %>'>",
+  "<textarea class='editContent' name='editContent' rows='8' cols='40'>",
+  "<%= content %>",
+  "</textarea>",
+  "<button type='button' class='submitEdit btn btn-warning'>Update</button>",
+  "</div>",
   "</article>"
 ].join("");
 templates.boo = [
